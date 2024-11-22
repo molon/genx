3fcb303af9ee4cc8839bee65dc4ec882e78b5ff1
molon <3739161+molon@users.noreply.github.com>
2024-11-22 14:35:00 +0800

    Update config.go

--- starter/config.go
+++ starter/config.go
@@ -8,7 +8,7 @@ import (
 type Config struct {
 	TargetDir          string `mapstructure:"targetDir" usage:"target directory to extract boilerplate"`
 	GoModule           string `mapstructure:"goModule" usage:"go module path to replace in boilerplate"`
-	BoilerplateZipFile string `mapstructure:"boilerplateZipFile" usage:"optional boilerplate zip file to extract, if not provided, use embedded boilerplate"`
+	BoilerplateZipFile string `mapstructure:"boilerplateZipFile" usage:"boilerplate zip file to extract, if not provided, use embedded boilerplate"`
 }
 
 func InitializeConfig(flagSet *pflag.FlagSet, envPrefix string) (configx.Loader[*Config], error) {
