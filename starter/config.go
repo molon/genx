7c5cf5ba628dbdca3e6f34d8a7348596892df6c6
molon <3739161+molon@users.noreply.github.com>
2024-11-23 17:17:05 +0800

    starter: config default embed yaml

--- starter/config.go
+++ starter/config.go
@@ -1,7 +1,12 @@
 package starter
 
 import (
+	"strings"
+
+	_ "embed"
+
 	"github.com/molon/genx/pkg/configx"
+	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 )
 
@@ -11,10 +16,13 @@ type Config struct {
 	BoilerplateZipFile string `mapstructure:"boilerplateZipFile" usage:"boilerplate zip file to extract, if not provided, use embedded boilerplate"`
 }
 
+//go:embed embed/default.yaml
+var defaultConfigYAML string
+
 func InitializeConfig(flagSet *pflag.FlagSet, envPrefix string) (configx.Loader[*Config], error) {
-	return configx.Initialize(flagSet, envPrefix, &Config{
-		TargetDir:          ".",
-		GoModule:           "",
-		BoilerplateZipFile: "",
-	})
+	def, err := configx.Read[*Config]("yaml", strings.NewReader(defaultConfigYAML))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load default config from embedded YAML")
+	}
+	return configx.Initialize(flagSet, envPrefix, def)
 }
